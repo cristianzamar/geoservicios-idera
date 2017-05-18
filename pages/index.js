4deba3369385508949f366ad1e0b3f6584e289b2
Cristian Zamar <cristianzamar@gmail.com>
2017-05-18 09:35:43 -0300

    limpieza de codigo comentado

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import fetch from 'isomorphic-unfetch';
 import Table from '../components/Table';
 import json from '../sources.json'
 
-// const Index = () => (
 export default () => (
   <div>
     <Table
@@ -14,21 +13,3 @@ export default () => (
     </Table>
   </div>
 )
-
-// Index.getInitialProps = async function() {
-//   // let data;
-//
-//   console.log(json);
-//
-//   // data = await fetch(
-//   //   'https://raw.githubusercontent.com/cristianzamar/geoservicios-idera/master/sources.json')
-//   // .then(function(res) {
-//   //   return res.json()
-//   // })
-//
-// // console.log(data);
-//
-//   return { sources: json }
-// }
-
-//export default Index
