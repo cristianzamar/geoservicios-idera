966225f93709b65007239cf7cc06becd4ad15ade
Cristian Zamar <cristianzamar@gmail.com>
2017-05-18 09:35:12 -0300

    agregue classes en los rows

--- components/RowNodo.js
+++ components/RowNodo.js
@@ -1,13 +1,24 @@
 let ultSuperior;
+let classTd;
 
-export default ({superior, datos})  => (
-  <tr>
-    <td>{ ultSuperior !== superior && (ultSuperior = superior) && superior }</td>
-    <td>{ datos.title }</td>
-    <td>
-      {datos.wms && <a href={datos.wms} target="_blank">WMS</a>}
-      {datos.wfs && <a href={datos.wfs} target="_blank">WFS</a>}
-    </td>
+export default ({superior, datos})  => {
 
-  </tr>
-)
+  //cambia estilo al cambiar el organismo superior
+  ultSuperior !== superior &&
+    (classTd = classTd ==='filas_oscuras' ? 'filas_claras' : 'filas_oscuras');
+
+  return (
+    <tr>
+      <td className={ classTd }>
+        { ultSuperior !== superior && (ultSuperior = superior) && superior }
+      </td>
+
+      <td className={ classTd }>{ datos.title }</td>
+
+      <td className={ classTd }>
+        { datos.wms && <a href={ datos.wms } target="_blank">WMS</a> }
+        { datos.wfs && <a href={ datos.wfs } target="_blank"><br/>WFS</a> }
+      </td>
+    </tr>
+  )
+}
